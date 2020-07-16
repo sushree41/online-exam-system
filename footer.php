eb29dbbd01273175c8e885fa23e7162b41f9e043
Sushree Samapika <57279639+sushree41@users.noreply.github.com>
2020-07-17 04:38:01 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+<div class="footer-copyright-area">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="footer-copy-right">
+                    <p>Designed and developed by:Sushree Samapika.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
+<script src="js/vendor/jquery-1.12.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
